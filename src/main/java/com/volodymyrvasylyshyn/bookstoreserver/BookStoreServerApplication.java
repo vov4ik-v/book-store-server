fb7ffdaed867510f6bde33fe72abc557391c38b1
vov4ik <volodymyr.vasylyshyn05@gmail.com>
2024-04-24 00:18:56 +0300

    Initial commit

---
+++ src/main/java/com/volodymyrvasylyshyn/bookstoreserver/BookStoreServerApplication.java
@@ -0,0 +1,13 @@
+package com.volodymyrvasylyshyn.bookstoreserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/volodymyrvasylyshyn/bookstoreserver/BookStoreServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volodymyrvasylyshyn.bookstoreserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
